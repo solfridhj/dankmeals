65c93aa8cb137904f75679d5f892fa633e4c710d
Solfrid Johansen <solfrid.johansen@hotmail.com>
2019-11-06 15:26:55 +0100

    Added image

--- scripts/navbar.js
+++ scripts/navbar.js
@@ -96,11 +96,11 @@ function recipe(){
                             </li>
         
                             <li>
-                                <a href="../about.html">About</a>
+                                <a href="../../about.html">About</a>
                             </li>
         
                             <li>
-                                <a href="../contact.html">Contact</a>
+                                <a href="../../contact.html">Contact</a>
                             </li>
                             <li>
 
